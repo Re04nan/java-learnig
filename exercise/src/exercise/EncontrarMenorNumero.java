73d350ec03cbaf81bb47c00c32291cad6ec1ed8f
Re04nan <renan04_marques@hotmail.com>
2021-05-05 23:36:37 -0300

    add exercise

--- exercise/src/exercise/EncontrarMenorNumero.java
+++ exercise/src/exercise/EncontrarMenorNumero.java
@@ -8,6 +8,17 @@ public class EncontrarMenorNumero {
 				100, 1, 5, -3, 200, 10, 3, 8, 7, 0
 		};
 		
+//		Integer counter = 0;
+//		Integer smallerNumber = null;
+//		
+//		while(counter < numbersList.length) {
+//			if (numbersList[0] > numbersList[counter]) {
+//				smallerNumber = numbersList[counter];
+//				numbersList[0] = smallerNumber;
+//			}
+//			counter++;
+//		}
+//		
 		Integer counter = 0;
 		Integer smallerNumber = Integer.MAX_VALUE;
 		
